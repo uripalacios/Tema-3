6a23e1b4254801a35e6b29f217770e920429f70f
uriel <uriel.palalo@educa.jcyl.es>
2021-10-15 15:13:20 +0000

    bisiestos

---
+++ codigo/compruebaMultiplos.php
@@ -0,0 +1,21 @@
+<h1>Comprueba multiplos</h1>
+<?php
+//Escriba un programa que pida un año y que escriba si es bisiesto o no.
+//Los años bisietos son múltiplos de 4, pero los multiplos de 100
+//no lo son,aunque los múltiplo de 400 sí. 
+$anio=(int)$_GET["ejemplo"];
+
+if(($anio%4) == 0){
+    if(($anio%100) != 0){
+        if(($anio%400) == 0){
+            echo "Año bisiesto";
+        }else{
+            echo "Año no bisiesto";
+        }
+    }
+}else{
+    echo "Año no bisiesto";
+}
+?>
+<br>
+<a href="codigo.php?paginaPHP=<?$pagina = basename($_SERVER['SCRIPT_FILENAME']);echo $pagina;?>">Ver el codigo</a>
