0740ad054d64e465f19b4ddbb8af6883bb830ffd
uriel <uriel.palalo@educa.jcyl.es>
2021-10-15 15:19:06 +0000

    bisiestos acabados

--- codigo/compruebaMultiplos.php
+++ codigo/compruebaMultiplos.php
@@ -4,15 +4,16 @@
 //Los años bisietos son múltiplos de 4, pero los multiplos de 100
 //no lo son,aunque los múltiplo de 400 sí. 
 $anio=(int)$_GET["ejemplo"];
-
+echo $anio;
+echo "<br>";
 if(($anio%4) == 0){
-    if(($anio%100) != 0){
+    if(($anio%100) == 0){
         if(($anio%400) == 0){
             echo "Año bisiesto";
         }else{
             echo "Año no bisiesto";
         }
-    }
+    }echo "Año bisiesto";
 }else{
     echo "Año no bisiesto";
 }
