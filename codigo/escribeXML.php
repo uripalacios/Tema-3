c7e86c4ff2c7fcf52628b54d2c9a27d06cacc880
uriel <uriel.palalo@educa.jcyl.es>
2021-11-16 19:42:37 +0000

    modificar xxml

--- codigo/escribeXML.php
+++ codigo/escribeXML.php
@@ -8,7 +8,34 @@
 
     //añadimos la raiz
     $raiz = $XML->appendChild($ElementoRaiz);
+    //creado etiqueta deporte
+    $deporte = $XML->createElement("Deporte");
+    //añadido
+    $ElementoRaiz->appendChild($deporte);
+    //creado etiqueta nombre
+    $nombre = $XML->createElement("Nombre");
+    //creo texto
+    $texto = $XML->createTextNode("Futbol");
+    $nombre->appendChild($texto);
+    $deporte->appendChild($nombre);
+    
+    //creado etiqueta nombre
+    $jug = $XML->createElement("Jugadores","11");
+    $deporte->appendChild($jug);
 
+    $deporte = $XML->createElement("Deporte");
+    //añadido
+    $ElementoRaiz->appendChild($deporte);
+    //creado etiqueta nombre
+    $nombre = $XML->createElement("Nombre");
+    //creo texto
+    $texto = $XML->createTextNode("Tenis");
+    $nombre->appendChild($texto);
+    $deporte->appendChild($nombre);
+    
+    //creado etiqueta nombre
+    $jug = $XML->createElement("Jugadores","1");
+    $deporte->appendChild($jug);
     //guardamos el fichero
     $XML->save("./ficheros/deportes.xml");
 ?>
---
+++ codigo/modXML.php
@@ -0,0 +1,36 @@
+<?php
+    $rutafichero = "./ficheros/deportes.xml";
+
+    if(file_exists($rutafichero)){
+        //Transforma el xml en un objeto de tipo simplexml
+        $xml = simplexml_load_file($rutafichero);
+        
+    }else{
+        exit;
+    }  
+
+    $dom = dom_import_simplexml($xml)->ownerDocument;
+
+    $deportes = $dom->firstChild;
+
+    foreach ($deportes->childNodes as $deporte) {
+        
+        //print_r ($deporte);
+        if($deporte->nodeName == "Deporte"){
+            foreach ($deporte->childNodes as $hijos) {
+                # code...
+                if($hijos->nodeName == "Nombre"){
+                    if($hijos->nodeValue == "Tenis"){
+                        $aux = $hijos;
+                        do
+                            $aux = $aux->nextSibling;
+                        while (!$aux->nodeName == "Jugadores");   
+                        $aux->nodeValue = 2;
+                    }
+                }
+            }
+        }
+    }
+
+    $dom->save($rutafichero);
+?>
